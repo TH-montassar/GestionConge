e8f062867250cf21f9c73767171182d99520f6a6
TH-montassar <montassar.themri@gmail.com>
2023-09-03 07:51:26 +0100

    test

--- front/src/pages/Loading/index.tsx
+++ front/src/pages/Loading/index.tsx
@@ -64,8 +64,7 @@ export const Loading = () => {
       </section>
 
       <footer
-        className="bg-gray-100 rounded-lg shadow dark:bg-gray-900 mt-12 w-full"
-        style={{ height: "15rem" }}
+        className="bg-gray-100 rounded-lg shadow dark:bg-gray-900 mt-12 w-full h-[12rem]"
       >
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
